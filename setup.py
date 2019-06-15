a3d75f15b58a76e6de0c874a76e7dfc0e7744cd3
Paul Winkler <paul.winkler@handshake.com>
2019-06-15 16:14:19 -0400

    Added tox for testing different django versions. Thanks Josh!!

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dj_related_signals",
+    url="https://github.com/slinkp/dj_related_signals",
+    version="0.1",
+    packages=find_packages(),
+    description="",
+    install_requires=[
+        "Django>=1.8",
+    ],
+    zip_safe=False,
+)
